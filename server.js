6214788e3e92c660fb8f36685d4b1bc50b0cef66
Eyüp Süyrüge <sueyruege_23@outlook.de>
2023-12-29 15:13:12 +0100

    laste

--- server.js
+++ server.js
@@ -1,52 +1,47 @@
-const express = require('express');
-const fs = require('fs').promises;
+
+import { initializeApp } from 'firebase/app';
+import { getDatabase, ref, set } from 'firebase/database';
+import express from 'express';
+
 const app = express();
 const port = 3000;
 
-// Import the functions you need from the SDKs you need
-const dataApp = require('firebase/app');
-const db = require('firebase/database');
-
 const firebaseConfig = {
-  apiKey: "AIzaSyD0jPbcEJnq_MH0NtZn2h8EGVCEUTOStak",
-  authDomain: "datenweb-df16e.firebaseapp.com",
-  databaseURL: "https://datenweb-df16e-default-rtdb.firebaseio.com",
-  projectId: "datenweb-df16e",
-  storageBucket: "datenweb-df16e.appspot.com",
-  messagingSenderId: "375576281180",
-  appId: "1:375576281180:web:e0a5d4d0a3739a8b232699"
+    apiKey: "AIzaSyD0jPbcEJnq_MH0NtZn2h8EGVCEUTOStak",
+    authDomain: "datenweb-df16e.firebaseapp.com",
+    databaseURL: "https://datenweb-df16e-default-rtdb.firebaseio.com",
+    projectId: "datenweb-df16e",
+    storageBucket: "datenweb-df16e.appspot.com",
+    messagingSenderId: "375576281180",
+    appId: "1:375576281180:web:e0a5d4d0a3739a8b232699"
 };
 
-// Initialize Firebase
-const dataAp = dataApp.initializeApp(firebaseConfig);
+const dataApp = initializeApp(firebaseConfig);
+const database = getDatabase(dataApp);
 
 function writeTo(name, mail, message) {
-    const reference = dataAp.database().ref("comment/");
+    const reference = ref(database, "comment/");
 
-    reference.set({
+    set(reference, {
         name: name,
         mail: mail,
         message: message
-    }, (error) => {
-        if (error) {
-            console.error('Error writing to the database:', error);
-        } else {
-            console.log('Data written to the database successfully.');
-        }
+    }).then(() => {
+        console.log('Data written to the database successfully.');
+    }).catch((error) => {
+        console.error('Error writing to the database:', error);
     });
 }
 
+app.get('/submitcomment/:name/:mail/:message', (req, res) => {
+    const name = req.params.name;
+    const mail = req.params.mail;
+    const message = req.params.message;
 
-
-    app.get('/submitcomment/:name/:mail/:message', (req, res) => {
-        const name = req.params.name;
-        const mail = req.params.mail;
-        const message = req.params.message;
-
-        writeTo(name,mail,message);
-
-    });
+    writeTo(name, mail, message);
+    res.send('Comment submitted successfully.');
+});
 
 app.listen(port, () => {
-    console.log(`Server läuft auf Port ${port}`);
+    console.log(`Server is running on port ${port}`);
 });
