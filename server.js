432611755eb4ba07d83da761ccd26207e8d8339e
Eyüp Süyrüge <89742883+EnsoSargent23@users.noreply.github.com>
2023-12-29 09:07:32 +0100

    server.js aktualisieren

--- server.js
+++ server.js
@@ -1,45 +1,36 @@
 const express = require('express');
-const fs = require('fs').promises; // Verwende fs.promises für asynchrone Dateioperationen
+const fs = require('fs').promises;
 const app = express();
 const port = 3000;
 
-// Initialisiere die Kommentarliste als leeres Array
 let commentsList = [];
 
-// Lese vorhandene Kommentare aus der JSON-Datei (falls vorhanden)
 async function readCommentsFromFile() {
     try {
         const data = await fs.readFile('data.json', 'utf8');
         commentsList = JSON.parse(data);
-        console.log("erfolgreich");
+        console.log("Datei erfolgreich gelesen");
     } catch (error) {
         console.error('Fehler beim Lesen der Daten aus data.json:', error);
     }
 }
 
-// Warte, bis die Kommentare aus der Datei gelesen wurden, bevor der Server startet
 readCommentsFromFile().then(() => {
-    // Endpoint zum Entgegennehmen von Kommentaren
     app.get('/submit-comment/:name/:mail/:message', (req, res) => {
-        // Entschlüsselung der Query-Parameter
         const name = req.params.name;
         const mail = req.params.mail;
         const message = req.params.message;
 
-        // Erstellen eines Kommentarobjekts
         const newComment = {
             name: name,
             mail: mail,
             message: message
         };
 
-        // Hinzufügen des Kommentars zur Liste
         commentsList.push(newComment);
-         console.log(commentsListd);
-        // Speichern der aktualisierten Kommentarliste in der JSON-Datei
+
         fs.writeFile('data.json', JSON.stringify(commentsList, null, 2), 'utf8')
             .then(() => {
-                // Rückgabe der aktuellen Kommentarliste als JSON
                 res.json({ success: true, message: 'Kommentar erfolgreich hinzugefügt', comments: commentsList });
             })
             .catch((err) => {
@@ -48,8 +39,9 @@ readCommentsFromFile().then(() => {
             });
     });
 
-    // Starten des Servers
     app.listen(port, () => {
         console.log(`Server läuft auf Port ${port}`);
     });
+}).catch((err) => {
+    console.error('Fehler beim Starten des Servers:', err);
 });
